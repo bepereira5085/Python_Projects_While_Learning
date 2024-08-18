16afd95ffb8bdd7c0998edc25d4619526ad4bd33
Bernardo Pereira Candido <164104265+bepereira5085@users.noreply.github.com>
2024-08-17 22:45:49 -0300

    3.exerc_nomes.py

--- 3.exerc_nomes.py
+++ 3.exerc_nomes.py
@@ -1,23 +1,34 @@
 try:
     print('')
-    print('FATOS DO NOME SEU E IDADE')
+    print('FATOS DO SEU NOME E IDADE')
     print('')
 
     nome = input('Digite seu nome: ')
     idade = input('Digite sua idade: ')
+    idade = int(idade)
     print('')
 
     if nome and idade:
 
         print(f'Seu nome é {nome}')
+        if idade >= 18:
+            print('Você é maior de idade')
+        else:
+            print('Você é menor de idade')
         print(f'Seu nome invertido é {nome[::-1]}')
         if ' ' in nome:
             print(f'Seu nome tem espaço(s)')
         else:
             print('Seu nome não tem espaços')
-        print(f'Seu nome tem {len(nome)} letras')
+        if ' ' in nome:
+            print('Seu nome é composto')
+            espacos = nome.count(' ')
+            print(f'Seu nome tem {len(nome) - espacos} letras')
+        else:
+            print('Seu nome não é composto')
+            print(f'Seu nome tem {len(nome)} letras')
         print(f'A primeira letra do seu nome é {nome[0]}')
-        print(f'A última letra do seu nome é {nome[-1]}')
+        print(f'A última letra do seu nome é {nome.upper()[-1]}')
     else:
         print('Por favor, digite nome e idade')
 except:
