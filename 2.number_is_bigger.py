791732607a0609a49fa258f937071a012fbab2f9
Bernardo Pereira Candido <164104265+bepereira5085@users.noreply.github.com>
2024-08-17 22:04:55 -0300

    2.number_is_bigger.py

--- 2.number_is_bigger.py
+++ 2.number_is_bigger.py
@@ -10,11 +10,12 @@ try:
     conv_2 = float(N2)
 
     if conv_1 > conv_2:
-        print(f'O primeiro número, {conv_1}, é maior que o segundo, {conv_2}')
+        print(f'The first number, {conv_1}, is bigger than the second one, {conv_2}')
     elif conv_2 > conv_1:
-        print(f'O segundo número, {conv_2}, é maior que o primeiro, {conv_1}')
+        print(f'The second number, {conv_2}, is bigger than the first one, {conv_1}')
     elif conv_1 == conv_2:
-        print(f'Os números são iguais, ou seja, {conv_1} = {conv_2}')
+        print(f'The numbers are equal, so {conv_1} = {conv_2}')
 
-except:
-    print("What you've done?, THIS IS NOT A NUMBER")
+except Exception as error_type:
+    print("What you've done?, one of them IS NOT A NUMBER")
+    print(error_type)
