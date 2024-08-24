4d423f383ac8c98f9bb5ce0f96a8351582947047
Bernardo Pereira Candido <164104265+bepereira5085@users.noreply.github.com>
2024-08-23 23:14:07 -0300

    Delete palindromos.py

--- palindromos.py
+++
@@ -1,9 +0,0 @@
-palavra = input('Insira uma palavra e descubra se é um palíndromo: ')
-
-if palavra.isalpha():
-    if (palavra[::-1]) == palavra:
-        print(f'{palavra} é um palíndromo')
-    else:
-        print(f'{palavra} não é um palíndromo')
-else:
-    print('Digite apenas letras e sem espaços')
