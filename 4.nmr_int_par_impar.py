9dd3e94e539647bf301ec235dcb3345ca45aefde
Bernardo Pereira Candido <164104265+bepereira5085@users.noreply.github.com>
2024-08-23 22:58:12 -0300

    Update 4.nmr_int_par_impar.py

--- 4.nmr_int_par_impar.py
+++ 4.nmr_int_par_impar.py
@@ -1,11 +1,1 @@
-try:
-    n1 = input('Digite um número inteiro: ')
-    n1_int = int(n1)
 
-    if n1_int % 2 == 0:
-        print('Esse número é par')
-    else:
-        print('Esse número é impar')
-
-except:
-    print('Você não digitou um número inteiro')
